d824c5e3c3f9737951b16576bd280589175e2987
ermira134 <78373924+ermira134@users.noreply.github.com>
2021-02-08 17:36:38 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'phpcrud';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+} 
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <title>CRUD APPLICATION</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+    <nav class="navtop">
+        <div>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+            <a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+        </div>
+    </nav>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include 'functions.php'
+// Your PHP code here.
+
+//Home Page template below.
+?>
+
+<?template_header()?>
+
+<div class="content">
+<div id="navtop">
+      <a class="nav"><img src="images.png"></a>
+
+</div>
+</div>
+
+<div class="content">
+
+	    <div id="nav">
+<a class="nav"><img src="image1.jpg"></a>
+<a class="nav"><img src="image2.jpg"></a>
+<a class="nav"><img src="images3.jpg"></a>
+<a class="nav"><img src="image4.jpg"></a>
+	</div>
+</div>
+
+
+
+<?template_footer()?>
